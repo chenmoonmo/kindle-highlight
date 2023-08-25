a93c4f8636720ff5ab96ca758d10de20aff48f94
chenmoonmo <chenmoonmo@gmail.com>
2023-08-26 01:19:47 +0800

    fix: try add assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@napi-rs/canvas"],
+    assetPrefix: isProd ? 'https://p-moonlight.zeabur.app' : undefined,
   }
 };
 
